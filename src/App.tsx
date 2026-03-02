c0cf1ace3588dbc041a4c8b2a053e575f2392a44
Adiiii3 <adityadavari666@gmail.com>
2026-03-02 18:49:05 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import WhatsAppWidget from './components/WhatsAppWidget';
+import Home from './pages/Home';
+import About from './pages/About';
+import Tours from './pages/Tours';
+import Gallery from './pages/Gallery';
+import Contact from './pages/Contact';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+      <div className="min-h-screen bg-earth-900 text-cream">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/tours" element={<Tours />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+        <WhatsAppWidget />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
